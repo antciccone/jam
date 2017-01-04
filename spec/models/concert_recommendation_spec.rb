735816b03b3dd47192a6e82b44370c0d0274ad42
antciccone <aciccone@bu.edu>
2017-01-03 18:17:31 -0700

    test pass for concert recommendation poro

--- spec/models/concert_recommendation_spec.rb
+++ spec/models/concert_recommendation_spec.rb
@@ -5,8 +5,21 @@ describe 'ConcertRecommendation' do
     it '.artist_concert_recommendation' do
       VCR.use_cassette("#recommendation") do
 
-        
+        artist = "Lettuce"
+        lat = "40.7128"
+        long = "-74.0059"
 
+        concert_rec = ConcertRecommendation.artist_concert_recommendation(artist, lat, long)
+
+        expect(concert_rec.first).to respond_to(:date)
+        expect(concert_rec.first).to respond_to(:title)
+        expect(concert_rec.first).to respond_to(:city)
+        expect(concert_rec.first).to respond_to(:ticket_link)
+        expect(concert_rec.first).to respond_to(:venue_location)
+        expect(concert_rec.last.name).to eq("The Greyboy Allstars")
+        expect(concert_rec.last.city).to eq("Brooklyn, NY")
+        expect(concert_rec.last.date).to eq("Friday, April 21, 2017 at 8:00PM")
+        expect(concert_rec.last.ticket_link).to eq("http://www.bandsintown.com/event/13489724/buy_tickets?app_id=jam&artist=The+Greyboy+Allstars&came_from=67")
       end
     end
   end
