30decf6ea3d517482b64eb2cb9bf7b67f05e6488
antciccone <aciccone@bu.edu>
2017-01-04 18:18:50 -0700

    user gets a flash message if there are no concerts in your area

--- spec/features/concerts/a_user_can_view_concerts_spec.rb
+++ spec/features/concerts/a_user_can_view_concerts_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe 'As a user' do
-  context 'when I click on an artist' do
+  context 'when I click on an artist and that artists has no concerts within 150 miles of me' do
     scenario 'I can view recommended concerts' do
-      VCR.use_cassette("#viewing_concerts") do
+      VCR.use_cassette("#concet_location") do
         visit "/"
         stub_omniauth
 
@@ -11,10 +11,25 @@ describe 'As a user' do
         expect(current_path).to eq(user_path(User.last))
 
         click_link 'Drake'
-        save_and_open_page
-        expect(current_path).to eq(concerts_path)
 
+        expect(current_path).to eq(concerts_path)
+        expect(page).to have_content("There are no concerts for Drake in your area")
       end
+    end
+  end
+
+    scenario 'I can view recommended concerts' do
+      VCR.use_cassette("#viewing_concerts") do
+      visit "/"
+      stub_omniauth
+
+      click_button 'Login with Spotify'
+      expect(current_path).to eq(user_path(User.last))
+
+      click_link 'Drake'
+
+      expect(current_path).to eq(concerts_path)
+      expect(page).to eq("Recommended Concerts")
     end
   end
 end
