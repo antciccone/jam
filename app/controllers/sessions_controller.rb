5cdd4c175852c6ee2181aa0e2b0486f24a6ffe37
antciccone <aciccone@bu.edu>
2017-01-05 12:42:35 -0700

    change to config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    require "pry"; binding.pry
     user = User.from_omniauth(request.env["omniauth.auth"], request.location.data["latitude"], request.location.data["longitude"])
     session[:uid] = user.uid
     user.user_refresh_token
