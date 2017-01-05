149d3dc35c78f6281b93377e36734824c7cd6772
antciccone <aciccone@bu.edu>
2017-01-04 18:08:06 -0700

    a user can log out spec

--- spec/features/user/a_user_can_log_out_spec.rb
+++ spec/features/user/a_user_can_log_out_spec.rb
@@ -4,13 +4,15 @@ require 'rails_helper'
 describe 'As a user' do
   context 'when I am logged in' do
     scenario 'I can log out' do
-      VCR.use_casette("#logout") do
+      VCR.use_cassette("#logout") do
         visit "/"
         stub_omniauth
 
         click_button 'Login with Spotify'
+        expect(current_path).to eq(user_path(User.last))
 
-
+        click_button 'Logout'
+        expect(current_path).to eq(root_path)
       end
     end
   end
