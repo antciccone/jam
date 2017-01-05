f88964d495ef4a1a65733c5b89d818d5f42383d2
antciccone <aciccone@bu.edu>
2017-01-05 12:38:45 -0700

    took out pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info, lat, long)
-    require "pry"; binding.pry
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid           = auth_info.uid
       new_user.latitude      = lat
