5c71f461b8d5598920270db2309472740dbd7369
antciccone <aciccone@bu.edu>
2017-01-11 17:58:14 -0700

    fix spelling

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -6,9 +6,9 @@ class ConcertsController < ApplicationController
     @concerts = ArtistConcerts.top_artists_concerts(params[:name], current_user.latitude, current_user.longitude)
     @concertrecommendation = ConcertRecommendation.artist_concert_recommendation(params[:name], current_user.latitude, current_user.longitude)
     @all_concerts = AllConcerts.all_artist_concerts(params[:name])
-    if @concerts == []
-      flash[:success] = "There are no concerts for #{params[:name]} in your area"
-    end
+    # if @concerts == []
+    #   flash[:success] = "There are no concerts for #{params[:name]} in your area"
+    # end
   end
 
   private
