03d937ea2a40aa5c7e09d58a4690e1ad8d6e0729
antciccone <aciccone@bu.edu>
2017-01-12 08:28:33 -0700

    a user can send an email

---
+++ spec/features/user/a_user_can_send_an_email_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I am view a concert' do
+    scenario 'I can send an email' do
+      VCR.use_cassette("#email") do
+        visit "/"
+        stub_omniauth
+
+        click_button 'Login with Spotify'
+        click_link 'Your Top Artists Concerts'
+        click_link 'Drake Concerts'
+
+        fill_in :email, with: "aciccone@bu.edu"
+        fill_in :subject, with: "test"
+        fill_in :message, with: "testing"
+        click_button "Send Email"
+
+        expect(current_path).to eq(top_artists_path)
+      end
+    end
+  end
+end
